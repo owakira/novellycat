b69f2bb223e04fa3877547f89952b0452bddbbfc
Damir Mirasov <damirmirasovmain@gmail.com>
2023-09-23 18:32:06 +0600

    Configure logging

--- src/__main__.py
+++ src/__main__.py
@@ -2,21 +2,23 @@ from sanic import Sanic
 
 from src.config import gather_config
 from src.gunicorn_app import GunicornApplication
+from src.logging import configure_logging
 
 
 def run_application() -> None:
-    # TODO: Configure logging
+    log_config = configure_logging('log.yml')
     config = gather_config()
 
     # TODO: Use application builder
-    app = Sanic('novellycat')
+    app = Sanic('novellycat', log_config=log_config)
 
     options = {
-        "bind": f"{config.server.address}:{config.server.port}",
-        "worker_class": "uvicorn.workers.UvicornWorker",
-        "reload": True,
-        "disable_existing_loggers": False,
-        "preload_app": True,
+        'bind': f'{config.server.address}:{config.server.port}',
+        'worker_class': 'uvicorn.workers.UvicornWorker',
+        'reload': True,
+        'disable_existing_loggers': False,
+        'preload_app': True,
+        'logconfig_dict': log_config,
     }
     gunicorn_app = GunicornApplication(app, options)
     gunicorn_app.run()
